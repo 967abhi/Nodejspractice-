9fbcbac896d81833b76036127332cd108175207b
Abhishek kumar singh <86023413+967abhi@users.noreply.github.com>
2023-10-08 13:13:58 -0500

    Adding nodejs in this file

---
+++ Global.js
@@ -0,0 +1,7 @@
+//Global variable
+console.log(__dirname + "This is the directory name ");
+console.log(__filename + "This is the File Name ");
+const myInterval=setInterval(()=>{
+    console.log("Hello Abhishek " + "count")
+},1000);
+clearInterval(myInterval());
---
+++ app.js
@@ -0,0 +1,12 @@
+const amount=9;
+
+
+if(amount<10){
+    console.log("small Number ");
+
+}
+else{
+    console.log("Higher Number ");
+
+}
+console.log(`Hey it's my first Node App`);
